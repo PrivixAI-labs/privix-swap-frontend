6e6793a6a8f87cf7aed46fb3148481c0d355c346
root <root@NLG-SAKIB.>
2025-03-12 21:01:54 +0600

    changed logo

--- psc-ui-kit/src/components/Svg/Icons/Logo.tsx
+++ psc-ui-kit/src/components/Svg/Icons/Logo.tsx
@@ -4,7 +4,7 @@ import { SvgProps } from "../types";
 const Icon: React.FC<SvgProps> = (props) => {
   return (
     <img
-      src="https://ipfs.io/ipfs/bafybeiemthc33jrn6xpguobposngtgoyfpf5irpxbyil53j6gx2ipwnre4/omega-4926.svg" // Replace with the path to your PNG file
+      src="https://ipfs.io/ipfs/bafybeia7oc3dakoaajdvuenirppwugsdbzwwiqngkbeffi46btp52ecvbq/new-logo-8390.svg" // Replace with the path to your PNG file
       alt="Logo" // Provide an appropriate alt text for accessibility
       {...props} // Spread any additional props passed to the component
     />
--- psc-ui-kit/src/widgets/Menu/icons/Logo.tsx
+++ psc-ui-kit/src/widgets/Menu/icons/Logo.tsx
@@ -9,7 +9,7 @@ const Logo: React.FC<LogoProps> = ({ isDark, ...props }) => {
   const textColor = isDark ? "#FFFFFF" : "#000000";
   return (
     <img
-      src="https://ipfs.io/ipfs/bafybeiemthc33jrn6xpguobposngtgoyfpf5irpxbyil53j6gx2ipwnre4/omega-4926.svg" // Replace with the path to your PNG file
+      src="https://ipfs.io/ipfs/bafybeia7oc3dakoaajdvuenirppwugsdbzwwiqngkbeffi46btp52ecvbq/new-logo-8390.svg" // Replace with the path to your PNG file
       alt="Logo" // Provide an appropriate alt text for accessibility
       style={{ color: textColor }} // Example style usage
       {...props} // Spread any additional props passed to the component
